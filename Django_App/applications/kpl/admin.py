88047d121ff31c1b8e60155aa8c6b05c5e6e59fd
Doctor Schultz <tokbaev201536@gmail.com>
2019-11-23 15:31:30 +0600

    up
    UEFA Champions League game cycles added

--- Django_App/applications/kpl/admin.py
+++ Django_App/applications/kpl/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Club, GameCycle, Eu_club
+from .models import Club, GameCycle, Eu_club, Eu_cycle
 # Register your models here.
 
 admin.site.register(Club)
 admin.site.register(GameCycle)
 admin.site.register(Eu_club)
+admin.site.register(Eu_cycle)
